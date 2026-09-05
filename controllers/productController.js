72ad11bf5799e5e7aee6662edbaed538bc8ce985
Bruno CT <btogitndev@gmail.com>
2026-09-05 11:34:12 -0300

    feat: create PUT and DELETE routes for products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,24 @@
 const products = require('../data/products');
 
-// get
+// get all
 function getProducts(req, res) {
     res.json(products);
 }
 
+// get BYD
+function getProductById(req, res) {
+    const id = Number(req.params.id);
+
+    const product = products.find(product => product.id === id);
+
+    if (!product) {
+        return res.status(404).json({
+            message: 'Product not found'
+        });
+    }
+
+    res.json(product);
+}
 
 // post
 function createProduct(req, res) {
@@ -24,7 +38,49 @@ function createProduct(req, res) {
     res.status(201).json(product);
 }
 
+// upd
+function updateProduct(req, res) {
+    const id = Number(req.params.id);
+
+    const product = products.find(product => product.id === id);
+
+    if (!product) {
+        return res.status(404).json({
+            message: 'Product not found'
+        });
+    }
+
+    const { user, description, quantity, price } = req.body;
+
+    product.user = user;
+    product.description = description;
+    product.quantity = quantity;
+    product.price = price;
+
+    res.json(product);
+}
+
+// delete
+function deleteProduct(req, res) {
+    const id = Number(req.params.id);
+
+    const productIndex = products.findIndex(product => product.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({
+            message: 'Product not found'
+        });
+    }
+
+    products.splice(productIndex, 1);
+
+    res.status(204).send();
+}
+
 module.exports = {
     getProducts,
-    createProduct
+    createProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const {
     getProducts,
-    createProduct
+    createProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
 } = require('../controllers/productController');
 
 const router = express.Router();
 
 router.get('/', getProducts);
 router.post('/', createProduct);
+router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
